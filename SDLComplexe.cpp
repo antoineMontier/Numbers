9e80159484a802532502ff2e14ba475132b88da5
Antoine <antoine.montier@sfr.fr>
2023-07-25 16:59:52 +0200

    graduation system is fine now

--- SDLComplexe.cpp
+++ SDLComplexe.cpp
@@ -3,80 +3,81 @@
 using namespace std;
 
 SDLComplexe::SDLComplexe() : SDL_Screen(1080, 720, "Complexe", 30.0) {
-    cList = new LinkedList<Complexe>();
+	cList = new LinkedList<Complexe>();
 }
 
 void SDLComplexe::run(){
-    SDLComplexe sc;
-    while(sc.isRunning()){
-        sc.bg(255);
-        sc.setColor(0);
-        sc.drawAxis();
-        sc.refreshAndDetailsAndEvents();
-    }
+	SDLComplexe sc;
+	while(sc.isRunning()){
+		sc.bg(255);
+		sc.setColor(0);
+		sc.drawAxis();
+		sc.refreshAndDetailsAndEvents();
+	}
 }
 
 void SDLComplexe::events(){
-    while (SDL_PollEvent(&e))
-        { // reads all the events (mouse moving, key pressed...)        //possible to wait for an event with SDL_WaitEvent
-            switch (e.type)
-            {
-            case SDL_WINDOWEVENT:
-                if (e.window.event == SDL_WINDOWEVENT_RESIZED)
-                    updateSize();
-                break;
-            case SDL_QUIT:
-                program_running = false; // quit the program if the user closes the window
-                break;
+	while (SDL_PollEvent(&e))
+		{ // reads all the events (mouse moving, key pressed...)        //possible to wait for an event with SDL_WaitEvent
+			switch (e.type)
+			{
+			case SDL_WINDOWEVENT:
+				if (e.window.event == SDL_WINDOWEVENT_RESIZED)
+					updateSize();
+				break;
+			case SDL_QUIT:
+				program_running = false; // quit the program if the user closes the window
+				break;
 
-            case SDL_KEYDOWN: // SDL_KEYDOWN : hold a key            SDL_KEYUP : release a key
-                switch (e.key.keysym.sym){ // returns the key ('0' ; 'e' ; 'SPACE'...)
+			case SDL_KEYDOWN: // SDL_KEYDOWN : hold a key            SDL_KEYUP : release a key
+				switch (e.key.keysym.sym){ // returns the key ('0' ; 'e' ; 'SPACE'...)
 
-                case SDLK_ESCAPE:
-                    program_running = false; // escape the program if user presses esc
-                    break;
+				case SDLK_ESCAPE:
+					program_running = false; // escape the program if user presses esc
+					break;
 
-                default:
-                    break;
-                }
+				default:
+					break;
+				}
 
-            case SDL_KEYUP:
-                switch (e.key.keysym.sym){
+			case SDL_KEYUP:
+				switch (e.key.keysym.sym){
 
-                    default:
-                        break;
-                }
+					default:
+						break;
+				}
 
-            default:
-                break;
-            }
-        }
+			default:
+				break;
+			}
+		}
 }
 
 void SDLComplexe::drawAxis(){
-    // number of graduations between a border of the screen and the origin
-    unsigned int graduation_count = 5;
-    
-    // === horizontal axis
+	// number of graduations between a border of the screen and the origin
+	unsigned int graduation_count = 5;
+	
+	// === horizontal axis
 
-    // axis
-    this->line(W()/2, 0, W()/2, H());
-    // arrow
-    this->filledTriangle(W()-10, int(H()/2)-10, W()-10, int(H()/2)+10, W(), int(H()/2));
-    
+	// axis
+	this->line(W()/2, 0, W()/2, H());
+	// arrow
+	this->filledTriangle(W()-10, int(H()/2)-10, W()-10, int(H()/2)+10, W(), int(H()/2));
+	
 
-    // === vertical axis
+	// === vertical axis
 
-    // axis
-    this->line(0, H()/2, W(), H()/2);
-    //arrow
-    this->filledTriangle(W()/2-10, 10, W()/2+10, 10, W()/2, 0);
+	// axis
+	this->line(0, H()/2, W(), H()/2);
+	//arrow
+	this->filledTriangle(W()/2-10, 10, W()/2+10, 10, W()/2, 0);
 
-    // === graduations
-   // for(int i = )
-    for( int i = int(W()/(graduation_count*2 + 2)) ; i < W() ; i += int(W()/(graduation_count*2 + 2)) ) if(i != W()/2) this->line(i, int(H()/2) - 5, i, int(H()/2) + 5);
-
-    for( int i = int(H()/(graduation_count*2 + 2)) ; i < H() ; i += int(H()/(graduation_count*2 + 2)) ) this->line(int(W()/2) - 5, i, int(W()/2) + 5, i);
-
+	// === graduations
+	for(int i = 1 ; i <= graduation_count*2 + 1 ; i++) {
+		if(i == graduation_count + 1) continue;
+		this->line(i*int(W()/(graduation_count*2 + 2)), int(H()/2) - 5, i*int(W()/(graduation_count*2 + 2)), int(H()/2) + 5); // horizontal
+		this->line(int(W()/2) - 5, i*int(H()/(graduation_count*2 + 2)), int(W()/2) + 5, i*int(H()/(graduation_count*2 + 2))); // vertical
+	}
+	
 
 }
